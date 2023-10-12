30a62a543fdbd8b3d7afc46e4c25e138a0a91321
tantalumm <tandemon45@gmail.com>
2023-10-12 13:32:35 +0700

    profile_back

---
+++ server/router/profiles.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const User = require("../models/users.js");
+const Art_auction = require("../models/bidArt.js"); //art for collection&auction
+const Art_buy = require("../models/sellArt.js"); // art for collection
+const Art_sell = require("../models/sellArt.js"); // art for artwork
+const mongoose = require("mongoose");
+const router = express.Router();
+
+router.get("/:_id", (req, res) => {
+  //get info user profile
+  User.findById({ _id: req.params._id })
+    .then((data) => {
+      res.json(data); // just showing data id
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error" });
+    });
+});
+
+// ต้องไปเช็ค user in owner id 
+router.get("/:_id/artwork", (req, res) => {
+  Art_sell.find({ _id: req.params._id })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error" });
+    });
+});
+
+router.get("/:_id/collection", (req, res) => {
+  Art_buy.findById({ _id: req.params._id })
+    .then((data) => {
+      res.json(data); // just showing collection id
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error" });
+    });
+});
+
+// art for auction
+router.get("/:_id/auction", (req, res) => {
+  Art_auction.find({ _id: req.params._id })
+    .then((data) => {
+      res.json(data); // just showing auction id
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error" });
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const db_connect = require("./db/db.js");
 const generalRouter = require("./router/general.js");
+const profiles = require("./router/profiles.js");
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use("/profiles",profiles);
 
 db_connect()
   .then(() => {
