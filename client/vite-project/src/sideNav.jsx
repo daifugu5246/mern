b19601fba2720b466a0b3b32d397ce0ae00b084e
Vipadaaa <64010808@kmitl.ac.th>
2023-10-12 12:03:07 +0700

    modal_addPic

--- client/vite-project/src/sideNav.jsx
+++ client/vite-project/src/sideNav.jsx
@@ -1,31 +1,32 @@
 import { useEffect } from "react";
-import noti from "./assets/noti.png"
-import setting from "./assets/setting.png"
-import question from "./assets/question.png"
-import axios from 'axios'
+import noti from "./assets/noti.png";
+import setting from "./assets/setting.png";
+import question from "./assets/question.png";
+import axios from "axios";
 import { useLoginContext } from "./context/loginContext";
 import { Link, useNavigate } from "react-router-dom";
 
-axios.defaults.baseURL = 'http://localhost:5000'
-
+axios.defaults.baseURL = "http://localhost:5000";
 
 function Circle(data) {
-
   function hoverCircle() {
-    const idcircle = document.getElementById(data.title)
-    idcircle.style.transition = "all 0.2s"
+    const idcircle = document.getElementById(data.title);
+    idcircle.style.transition = "all 0.2s";
     idcircle.style.backgroundColor = "#FCD3B4";
   }
   function normalCircle() {
-    const idcircle = document.getElementById(data.title)
-    idcircle.style.transition = "all 0.2s"
+    const idcircle = document.getElementById(data.title);
+    idcircle.style.transition = "all 0.2s";
     idcircle.style.backgroundColor = "#ffeee1";
   }
-  useEffect(() => {
-
-  })
+  useEffect(() => {});
   return (
-    <div id={data.path} className={"text-center " + data.d} onMouseOver={() => hoverCircle()} onMouseOut={() => normalCircle()}>
+    <div
+      id={data.path}
+      className={"text-center " + data.d}
+      onMouseOver={() => hoverCircle()}
+      onMouseOut={() => normalCircle()}
+    >
       <Link to={"/" + data.path}>
         <div
           id={data.title}
@@ -45,11 +46,11 @@ function Circle(data) {
 }
 
 function SideNav() {
-  const { isLoggedin } = useLoginContext()
+  const { isLoggedin } = useLoginContext();
   useEffect(() => {
-    const user = document.getElementById("profile")
-    if (!isLoggedin) user.style.display = "none"
-    else if (isLoggedin) user.style.display = "block"
+    const user = document.getElementById("profile");
+    if (!isLoggedin) user.style.display = "none";
+    else if (isLoggedin) user.style.display = "block";
   });
   return (
     <div
@@ -76,7 +77,6 @@ function SideNav() {
         data-bs-target="#exampleModal"
       >
         <div id="artwork" className="text-center ">
-
           <div
             id="Artwork"
             className="container-fluid rounded-circle border border-dark  p-3 d-flex justify-content-center border bgCircle"
@@ -85,7 +85,7 @@ function SideNav() {
               className="img-fluid"
               src="https://cdn-icons-png.flaticon.com/512/10493/10493896.png"
               alt="img"
-              style={{ objectFit: "cover", maxWidth: "25px" }}
+              style={{  objectFit: "cover", maxWidth: "25px" , radius: "45px",border: "1px solid #000"}}
             />
           </div>
           <p>Artwork</p>
@@ -100,21 +100,21 @@ function SideNav() {
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel"></h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-
-            <div className="modal-body">
+        <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered madalFrame">
+          <div className="modal-content"
+          style={{ width: "800px", height: "918px" ,
+          background:"#FFD7B9", justifyContent: "center",
+          shadow:" 2px 3px 15px 5px rgba(0, 0, 0, 0.25)", borderRadius:"45px" , padding: "0px"}}>
+            
+            <div className="modal-body"
+                 style={{ margin:"0px" , padding: "0px"}}>
               <ul className="nav justify-content-evenly">
-                <li className="nav-item" role="presentation">
+                <li
+                  className="nav-item display-flex"
+                  role="presentation"
+                  id="pill-sell-item"
+      
+                >
                   <button
                     className="nav-link active"
                     id="pills-sell-tab"
@@ -124,6 +124,9 @@ function SideNav() {
                     role="tab"
                     aria-controls="pills-sell"
                     aria-selected="true"
+                    style={{ height: "61px"}}
+                    /*style={{ height: "61px", radius: "45px 0px 0px 0px", background: "#CF594D",
+                             borderRadius: "45px 0px 0px 0px" , margin:"0px" , padding: "0px" }} */
                   >
                     Sell
                   </button>
@@ -138,6 +141,7 @@ function SideNav() {
                     role="tab"
                     aria-controls="pills-auction"
                     aria-selected="false"
+                    style={{ height: "61px"}}
                   >
                     Auction
                   </button>
@@ -150,13 +154,167 @@ function SideNav() {
                   role="tabpanel"
                   aria-labelledby="pills-sell-tab"
                 >
-                  aaaaaaaaaaaaaa
-                  <img
-                    src="https://i.pinimg.com/564x/1c/c2/40/1cc2408849475c4fe0963566ad520fea.jpg"
-                    className="rounded mx-auto d-block"
-                    alt="img"
-                  ></img>
+
+                { /* add pic */}
+                <button
+                      className="uploadPicButton"
+                      style={{ width: "415px" , height: "269px" , flexShrink: "0" , borderRadius: "45px",
+                      background: "rgba(15, 12, 12, 0.14)", margin:"20px 34px 20px 34px"}}
+                    
+                    >
+                      <img
+                        className="img-fluid"
+                        src="http://drive.google.com/uc?export=view&id=1cXj8wYhFalP1opSKwAq_zeSMg9oHbwmp"
+                        alt="img"
+                        style={{  objectFit: "cover", maxWidth: "25px" , radius: "45px"}}
+
+                      />
+                    </button>
+
+                    <div className="addPicForm" style={{ marginBottom : "20px"}}>
+                      <label style={{ width: "110px" , height: "35px" ,borderRadius: "27px" , background: "#FD997F" ,
+                      color: "#0F0C0C",
+                      textAlign: "center",
+                      fontSize: "17px",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      marginLeft : "40px",
+                      paddingTop : "5px"}}
+                      >Art title</label>
+                      <input style={{ width: "275px" , height: "35px" ,
+                      paddingLeft : "15px",
+                      paddingRight : "15px",
+                      marginLeft : "25px",
+                      borderRadius: "27px" ,
+                      borderTop: "1px solid #000",
+                      borderRight: "1px solid #000",
+                      borderBottom: "2px solid #000",
+                      borderLeft: "2px solid #000",
+                      background: "#FFEEE1"}}
+                      type="text" placeholder="Limit 50 letters"></input>
+                    </div>
+
+                    <div className="addPicForm" style={{ marginBottom : "20px"}}>
+                      <label style={{ width: "110px" , height: "35px" ,borderRadius: "27px" , background: "#FD997F" ,
+                      color: "#0F0C0C",
+                      textAlign: "center",
+                      fontSize: "17px",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      marginLeft : "40px",
+                      paddingTop : "5px"}}
+                      >Tag</label>
+                      <input style={{ width: "275px" , height: "35px" ,
+                      paddingLeft : "15px",
+                      paddingRight : "15px",
+                      marginLeft : "25px",
+                      borderRadius: "27px" ,
+                      borderTop: "1px solid #000",
+                      borderRight: "1px solid #000",
+                      borderBottom: "2px solid #000",
+                      borderLeft: "2px solid #000",
+                      background: "#FFEEE1"}}
+                      type="text" placeholder="Limit 50 letters"></input>
+                    </div>
+
+                    <div className="addPicForm" style={{ marginBottom : "20px"}}>
+                      <label style={{ width: "110px" , height: "35px" ,borderRadius: "27px" , background: "#FD997F" ,
+                      color: "#0F0C0C",
+                      textAlign: "center",
+                      fontSize: "17px",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      marginLeft : "40px",
+                      paddingTop : "5px"}}
+                      >Description</label>
+                      <input style={{ width: "275px" , height: "113px" ,
+                      paddingLeft : "15px",
+                      paddingRight : "15px",
+                      marginLeft : "25px",
+                      borderRadius: "27px" ,
+                      borderTop: "1px solid #000",
+                      borderRight: "1px solid #000",
+                      borderBottom: "2px solid #000",
+                      borderLeft: "2px solid #000",
+                      background: "#FFEEE1"}}
+                      type="text" placeholder="Limit 50 letters"></input>
+                    </div>
+
+                    <div className="addPicForm" style={{ marginBottom : "20px" , display:"flex"}}>
+                      <label style={{ width: "110px" , height: "35px" ,borderRadius: "27px" , background: "#FD997F" ,
+                      color: "#0F0C0C",
+                      textAlign: "center",
+                      fontSize: "17px",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      marginLeft : "40px",
+                      paddingTop : "5px"}}
+                      >Price</label>
+                      <input style={{ width: "240px" , height: "35px" ,
+                      paddingLeft : "15px",
+                      paddingRight : "15px",
+                      marginLeft : "25px",
+                      borderRadius: "27px" ,
+                      borderTop: "1px solid #000",
+                      borderRight: "1px solid #000",
+                      borderBottom: "2px solid #000",
+                      borderLeft: "2px solid #000",
+                      background: "#FFEEE1"}}
+                      type="text"></input>
+                      {/*   leaf */}
+                      <img
+                        className="img-fluid"
+                        src="http://drive.google.com/uc?export=view&id=1ZiBlvrC7bcBvaJwdNVErH4gZRbf_X7N4"
+                        alt="img"
+                        style={{  objectFit: "cover", maxWidth: "35px", marginLeft: "10px"}}
+                      />
+                    </div>
+
+                    <div className="addPicForm" style={{ marginBottom : "20px"}}>
+                      <input type="radio" style={ {marginLeft:"40px"}}></input>
+                      <label style={{ width: "110px" , height: "35px" ,borderRadius: "27px" , background: "#FD997F" ,
+                      color: "#0F0C0C",
+                      textAlign: "center",
+                      fontSize: "14px",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      marginLeft : "6px",
+                      paddingTop : "5px"}}
+                      >Limit Amount</label>
+                      <input style={{ width: "240px" , height: "35px" ,
+                      paddingLeft : "15px",
+                      paddingRight : "15px",
+                      marginLeft : "25px",
+                      borderRadius: "27px" ,
+                      borderTop: "1px solid #000",
+                      borderRight: "1px solid #000",
+                      borderBottom: "2px solid #000",
+                      borderLeft: "2px solid #000",
+                      background: "#FFEEE1"}}
+                      type="text"></input>
+                      <label style={{ width: "3px" , height: "35px" ,
+                      color: "#0F0C0C",
+                      textAlign: "center",
+                      fontSize: "20px",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      marginLeft : "6px",
+                      paddingTop : "5px"}}
+                      >pcs</label>
+                  </div>
+
+                  {/*horizontal line */}
+                  <div className="addPicForm" style={{ marginBottom : "0px" , display:"flex"}}>
+                    <hr style={{width: "95%",
+                      marginLeft: "auto",
+                      marginRight: "auto"}} >
+                    </hr>
+                  </div>
+
                 </div>
+
+                { /* sell body */ }
+
                 <div
                   className="tab-pane fade"
                   id="pills-auction"
@@ -183,43 +341,29 @@ function SideNav() {
                   </form>
                 </div>
               </div>
-              a b c content to show the scrolling behavior for modals. We use
-              repeated line breaks to demonstrate how content can exceed minimum
-              inner height, thereby showing inner scrolling. When content
-              becomes longer than the prefedined max-height of modal, content
-              will be cropped and scrollable within the modal. This content
-              should appear at the bottom after you scroll a b c content to show
-              the scrolling behavior for modals. We use repeated line breaks to
-              demonstrate how content can exceed minimum inner height, thereby
-              showing inner scrolling. When content becomes longer than the
-              prefedined max-height of modal, content will be cropped and
-              scrollable within the modal. This content should appear at the
-              bottom after you scroll a b c content to show the scrolling
-              behavior for modals. We use repeated line breaks to demonstrate
-              how content can exceed minimum inner height, thereby showing inner
-              scrolling. When content becomes longer than the prefedined
-              max-height of modal, content will be cropped and scrollable within
-              the modal. This content should appear at the bottom after you
-              scroll
-            </div>
-
-            
-            <div className="modal-footer-centered">
-
-            <div className="text-center">
-              <button
-                type="button"
-                className="btn btn-dark"
-                data-bs-dismiss="modal"
-              >
-                Publish
-              </button>
-            </div>
               
-            
+             
             </div>
-            
 
+            <div className="modal-footer-centered"
+            style={{height: "70px"}}>
+              <div className="text-center ">
+                <button
+                  type="button"
+                  className="btn"
+                  data-bs-dismiss="modal"
+                  
+                  style={{backgroundImage: "linear-gradient(to left, #F5C6F7 , #FF9C7D )",
+                  border: "solid 2px black",boxShadow: "2px 2px black",borderRadius: "45px",
+                  width: "100px",
+                  marginTop: "8px"
+                  }}
+                  
+                >
+                  Publish
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -230,28 +374,72 @@ function SideNav() {
         path="trend"
       />
 
-      <div id="AlertNav" className="d-flex align-items-center flex-column rounded-pill py-1 mb-1">
-        <button type="button" className="btn border-0" data-bs-toggle="modal" data-bs-target="#noti">
-          <img className="img-fluid" src={noti} alt="img" style={{ objectFit: "cover", maxWidth: "20px" }} />
+      <div
+        id="AlertNav"
+        className="d-flex align-items-center flex-column rounded-pill py-1 mb-1"
+      >
+        <button
+          type="button"
+          className="btn border-0"
+          data-bs-toggle="modal"
+          data-bs-target="#noti"
+        >
+          <img
+            className="img-fluid"
+            src={noti}
+            alt="img"
+            style={{ objectFit: "cover", maxWidth: "20px" }}
+          />
         </button>
 
-        <button type="button" className="btn border-0" data-bs-toggle="modal" data-bs-target="#noti">
-          <img className="img-fluid" src={setting} alt="img" style={{ objectFit: "cover", maxWidth: "20px" }} />
+        <button
+          type="button"
+          className="btn border-0"
+          data-bs-toggle="modal"
+          data-bs-target="#noti"
+        >
+          <img
+            className="img-fluid"
+            src={setting}
+            alt="img"
+            style={{ objectFit: "cover", maxWidth: "20px" }}
+          />
         </button>
 
-        <button type="button" className="btn border-0 pb-3" data-bs-toggle="modal" data-bs-target="#noti">
-          <img className="img-fluid" src={question} alt="img" style={{ objectFit: "cover", maxWidth: "20px" }} />
+        <button
+          type="button"
+          className="btn border-0 pb-3"
+          data-bs-toggle="modal"
+          data-bs-target="#noti"
+        >
+          <img
+            className="img-fluid"
+            src={question}
+            alt="img"
+            style={{ objectFit: "cover", maxWidth: "20px" }}
+          />
         </button>
-
       </div>
 
-
-      <div className="modal fade" id="noti" tabIndex="-1" aria-labelledby="ModalNoti" aria-hidden="true">
+      <div
+        className="modal fade"
+        id="noti"
+        tabIndex="-1"
+        aria-labelledby="ModalNoti"
+        aria-hidden="true"
+      >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="ModalNoti">Notification</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h5 className="modal-title" id="ModalNoti">
+                Notification
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
             <div className="modal-body" style={{ minHeight: "500px" }}>
               ...
@@ -260,8 +448,6 @@ function SideNav() {
         </div>
       </div>
     </div>
-
-
   );
 }
 
