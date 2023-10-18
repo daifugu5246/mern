d17eb5f1b5ffee8c1e2b57f3672f9248aa302c9e
Saranthorn0827 <64010827@kmitl.ac.th>
2023-10-19 03:52:45 +0700

    Update lol

--- server/router/auction.js
+++ server/router/auction.js
@@ -105,13 +105,13 @@ router.get('/:img_id', (req, res) => {
         }).catch((err) => res.status(500).send("Error find image failed" + err));
 });
 
-router.get('get-owners/:owners_id', (req, res) => {
+router.get('/get-owners/:owners_id', (req, res) => {
     Users.findOne({ _id: req.params.owners_id })
-    .then((owners) => {
-        res.status(200).json({
-            owners_username: owners.username,
+        .then((owners) => {
+            res.status(200).json({
+                owners_username: owners.username,
+            });
         });
-    });
 });
 //[finish] อัพเดตค่า status ,current_price ให้กับ fornt-end จะเรียกใช้เมื่อต้องการเปลี่ยนสถานะ
 router.get('/:img_id/update', (req, res) => {
